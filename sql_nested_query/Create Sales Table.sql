21c9f5914f4f04e85795f8198de7917080218d01
billythomasalljoydata <138125451+billythomasalljoydata@users.noreply.github.com>
2023-06-30 12:35:31 -0400

    Update Create Sales Table.sql

--- sql_nested_query/Create Sales Table.sql
+++ sql_nested_query/Create Sales Table.sql
@@ -8,3 +8,22 @@ CREATE TABLE [dbo].[Sales](
 GO
 
 
+INSERT INTO [dbo].[Sales](Name, State, Sales)
+VALUES
+('BILL','SC',116),
+('BILL','SC',1000),
+('BILL','SC',225),
+('JOHN','GA',100),
+('JOHN','GA',95),
+('SUE','OH',80),
+('SUE','OH',90),
+('SUE','OH',87),
+('SUE','OH',40),
+('SALLY','FL',102),
+('SALLY','FL',105),
+('SALLY','FL',102),
+('JIM','SC',95),
+('JIM','SC',100)
+GO
+
+
